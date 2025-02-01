ea0efc1f05eb62d4212c9da398892e07c6e512a3
AlienMinus <dasmanasranjan2005@gmail.com>
2025-02-01 09:05:20 +0530

    modified:   02Hello.c

--- 02Hello.c
+++ 02Hello.c
@@ -5,6 +5,7 @@
     Description: This program prints "Hello World!" to the console and returns 0 to indicate successful execution.
 */
 
-#include <stdio.h> int main(){ printf("Hello World!"); return 0; }
+#include <stdio.h> 
+int main(){ printf("Hello World!"); return 0; }
 // Executing The Whole Program in a Single Line
 // It'll work but it's a very very bad Approach
